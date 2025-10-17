8f631ba75c9c4e10d17189c764efdfd82c2d185a
mohanreddynandaluru <mohanreddy232312@gmail.com>
2025-10-17 17:27:28 +0530

    deployment fixes

--- backend/io/socketIO.js
+++ backend/io/socketIO.js
@@ -5,7 +5,7 @@ let io;
 function initSocket(server) {
   io = new Server(server, {
     cors: {
-      origin: "http://localhost:5173",
+      origin: "https://unknown-rooms.vercel.app",
       methods: ["GET", "POST"],
       credentials: true,
     },
